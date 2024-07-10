d6a3185f60e0ed804d05245da21d607f04ea772b
kova <i.l.y.c.r.o.g.e.r.s.0.9.2.0@gmail.com>
2024-07-10 15:29:18 -0700

    feat(video): add multiple file types

--- v1/get_video.go
+++ v1/get_video.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/venqoi/racc-api/utils"
@@ -12,7 +13,19 @@ func GetRaccoonVideo(c *fiber.Ctx) error {
 	var wantsJSON = utils.WantsJSON(c)
 	randomIndex := utils.GetRandomIndexVideo()
 
-	bytes, err := os.ReadFile("./raccs/videos/racc" + fmt.Sprint(randomIndex) + ".mp4")
+	supportedExtensions := []string{".mp4", ".mov"}
+
+	var videoPath string
+	var bytes []byte
+	var err error
+
+	for _, ext := range supportedExtensions {
+		videoPath = fmt.Sprintf("./raccs/videos/racc%d%s", randomIndex, ext)
+		bytes, err = os.ReadFile(videoPath)
+		if err == nil {
+			break
+		}
+	}
 
 	c.Set("X-Raccoon-Video-Index", fmt.Sprint(randomIndex))
 
@@ -32,13 +45,18 @@ func GetRaccoonVideo(c *fiber.Ctx) error {
 		return c.JSON(utils.Response{
 			Success: true,
 			Data: utils.VideoStruct{
-				URL:   utils.BaseURL(c) + "/v1/video/" + fmt.Sprint(randomIndex),
+				URL:   utils.BaseURL(c) + "/v1/raccoon/video/" + filepath.Base(videoPath),
 				Index: randomIndex,
 				Alt:   utils.GetAlti(randomIndex),
 			},
 		})
 	}
 
-	c.Set("Content-Type", "video/mp4")
+	contentType := "video/mp4"
+	if filepath.Ext(videoPath) == ".mov" {
+		contentType = "video/quicktime"
+	}
+
+	c.Set("Content-Type", contentType)
 	return c.Send(bytes)
 }
