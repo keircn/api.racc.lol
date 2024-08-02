0bcf1cb71dd4872aeb82daadac1c881395ad83ac
kova 🪿 <i.l.y.c.r.o.g.e.r.s.0.9.2.0@gmail.com>
2024-08-01 23:10:52 -0700

    feat: Increment db after every request and add stats request
    Added MongoDB (optional) connection to store +1 after every request as well as add /stats/ as a request.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -13,10 +16,35 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/venqoi/racc-api/utils"
 	v1 "github.com/venqoi/racc-api/v1"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var requestCountCollection *mongo.Collection
+var isDbConnected bool
+var notConnectedMessages = []string{
+	"Not connected to database",
+	"a raccoon ate the database",
+	"the database is in the trash",
+}
 
 func main() {
 	godotenv.Load()
+
+	mongoURI := os.Getenv("MONGO_DB_URL")
+	if mongoURI != "" {
+		client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongoURI))
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Disconnect(context.TODO())
+
+		requestCountCollection = client.Database("racc").Collection("requestCounts")
+		isDbConnected = true
+	} else {
+		isDbConnected = false
+	}
 
 	raccImages, _ := os.ReadDir("raccs")
 	raccVideos, _ := os.ReadDir("raccs/videos")
@@ -58,15 +86,60 @@ func main() {
 			})
 		},
 		KeyGenerator: func(c *fiber.Ctx) string {
-			return c.GetReqHeaders()["X-Forwarded-For"]
+			xfwdFor := c.GetReqHeaders()["X-Forwarded-For"]
+			if len(xfwdFor) > 0 {
+				return strings.Join(xfwdFor, ",")
+			}
+			return c.IP()
 		},
 	}))
+
+	app.Use(func(c *fiber.Ctx) error {
+		if c.Path() != "/favicon.ico" && isDbConnected {
+			_, err := requestCountCollection.UpdateOne(context.TODO(), bson.M{"_id": "requestCount"}, bson.M{"$inc": bson.M{"count": 1}}, options.Update().SetUpsert(true))
+			if err != nil {
+				return err
+			}
+		}
+		return c.Next()
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.JSON(utils.Response{
 			Success: true,
 			Message: "trash panda discovered! you've found the api.",
 		})
+	})
+
+	app.Get("/stats", func(c *fiber.Ctx) error {
+		if isDbConnected {
+			var result struct {
+				Count int `bson:"count"`
+			}
+			err := requestCountCollection.FindOne(context.TODO(), bson.M{"_id": "requestCount"}).Decode(&result)
+			if err != nil {
+				return err
+			}
+			return c.JSON(utils.StatsResponse{
+				Success:           true,
+				Message:           "stats for the trash panda api :) ",
+				Requests:          result.Count,
+				Images:            utils.NUMBER_OF_IMAGES,
+				Videos:            utils.NUMBER_OF_VIDEOS,
+				TransparentImages: utils.NUMBER_OF_TRANS,
+			})
+		} else {
+			rand.Seed(time.Now().UnixNano())
+			randomMessage := notConnectedMessages[rand.Intn(len(notConnectedMessages))]
+			return c.JSON(utils.StatsResponse{
+				Success:           true,
+				Message:           "stats for trash panda api :)",
+				Requests:          randomMessage,
+				Images:            utils.NUMBER_OF_IMAGES,
+				Videos:            utils.NUMBER_OF_VIDEOS,
+				TransparentImages: utils.NUMBER_OF_TRANS,
+			})
+		}
 	})
 
 	v1Group := app.Group("/v1")
